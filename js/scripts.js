54859d867c8a64929c49f2650eb3ff2baa618786
Sonja Scheller <sscheller98@live.com>
2019-06-13 23:35:55 -0700

    Updated
    *Added nav ,nav css, nav js
    *Added an aside with my projects
    used flex box properties

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 console.log('OK');
+$(document).ready(function() {
+	   
+    // JQUERY NAV TOGGLE
+    $('#menu').bind('click',function(event){
+        $('#mainnav ul').slideToggle();
+    });
+
+    $(window).resize(function(){  
+        var w = $(window).width();  
+        if(w > 768) {  
+            $('#mainnav ul').removeAttr('style');  
+        }  
+    });
+      
+  });
+
+
+  
